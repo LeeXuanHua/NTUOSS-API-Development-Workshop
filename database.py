437333bddfb6bd68b466a2f2f56f7bbb8f14957a
Lee Xuan Hua <46639916+LeeXuanHua@users.noreply.github.com>
2022-06-09 13:38:02 +0800

    Negligible change

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+
 # Database Setup
 SQLALCHEMY_DATABASE_URL = 'sqlite:///db.sqlite3'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
